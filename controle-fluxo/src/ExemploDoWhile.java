545dd861373564305bdaa179b7af762569e5fdf4
Bruno Gonçalves <bruuno_goncalves@outlook.com.br>
2024-06-02 16:35:04 -0300

    add: Exemplo DoWhile

--- controle-fluxo/src/ExemploDoWhile.java
+++ controle-fluxo/src/ExemploDoWhile.java
@@ -1,20 +1,8 @@
-import java.util.Random;
 public class ExemploDoWhile {
   public static void main(String[] args) {
-      System.out.println("Discando...");
-      
-      do {
-        //excutando repetidas vezes até alguém atender
-        System.out.println("Telefone tocando");
-      
-      }
-      while(tocando());
-      System.out.println("Alô !!!");
+    for(int x=1;x<=10;x++){
+      if(x % 2==0) //% operador que representa módulo de uma divisão
+          System.out.println(x);
   }
-  private static boolean tocando() {
-      boolean atendeu = new Random().nextInt(3)==1;
-      System.out.println("Atendeu? " + atendeu);
-      //negando o ato de continuar tocando
-      return ! atendeu;
   }
 }
