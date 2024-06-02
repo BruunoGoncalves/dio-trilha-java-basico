45c29a01115aca1d28adaeb9f58419dacbc7082f
Bruno Gonçalves <bruuno_goncalves@outlook.com.br>
2024-06-02 17:36:01 -0300

    fix: Adicionando try/catch

--- controle-fluxo/src/About.java
+++ controle-fluxo/src/About.java
@@ -5,25 +5,31 @@ public class About {
   public static void main(String[] args) {
     //criando o objeto scanner
     Scanner sc = new Scanner(System.in).useLocale(Locale.US);
-        
-    System.out.println("Digite seu nome");
-    String nome = sc.next();
-    
-    System.out.println("Digite seu sobrenome");
-    String sobrenome = sc.next();
+    try {
+      System.out.println("Digite seu nome");
+      String nome = sc.next();
+      
+      System.out.println("Digite seu sobrenome");
+      String sobrenome = sc.next();
 
-    System.out.println("Digite sua idade");
-    int idade = sc.nextInt();
-    
-    System.out.println("Digite sua altura");
-    double altura = sc.nextDouble();
+      System.out.println("Digite sua idade");
+      int idade = sc.nextInt();
+      
+      System.out.println("Digite sua altura");
+      double altura = sc.nextDouble();
 
-    
-    //imprimindo os dados obtidos pelo usuario
-    System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
-    System.out.println("Tenho " + idade + " anos ");
-    System.out.println("Minha altura é " + altura + "cm ");
+      
+      //imprimindo os dados obtidos pelo usuario
+      System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+      System.out.println("Tenho " + idade + " anos ");
+      System.out.println("Minha altura é " + altura + "cm ");
 
-    sc.close();
+      sc.close();
+    } catch (Exception e) {
+      // TODO: handle exception
+      System.out.println("Preencha o campo Idade e Altura com números.");
+
+    }    
+    
   }
 }
